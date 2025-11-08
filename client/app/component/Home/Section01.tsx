8a898e3193a5a814efdacbabdea816f78b70ce44
sajitha00 <sajithageevinda.sd@gmail.com>
2025-11-08 14:43:25 +0530

    added hero text aniamtion

--- client/app/component/Home/Section01.tsx
+++ client/app/component/Home/Section01.tsx
@@ -1,37 +1,39 @@
-'use client'
-import React, { useState, useEffect } from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
 
-const wordToType = "Collection"
+const wordToType = "Collection";
 
 function Section01() {
-  const [displayText, setDisplayText] = useState('')
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [displayText, setDisplayText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     if (currentIndex < wordToType.length) {
       const timeout = setTimeout(() => {
-        setDisplayText(prev => prev + wordToType[currentIndex])
-        setCurrentIndex(prev => prev + 1)
-      }, 100) // Typing speed: 100ms per character
+        setDisplayText((prev) => prev + wordToType[currentIndex]);
+        setCurrentIndex((prev) => prev + 1);
+      }, 100); // Typing speed: 100ms per character
 
-      return () => clearTimeout(timeout)
+      return () => clearTimeout(timeout);
     }
-  }, [currentIndex])
+  }, [currentIndex]);
 
   return (
-    <div data-aos="fade-up" data-aos-duration="2000">
-      <div className='-mt-15 lg:mt-[-50px]'>
-        <div className='title text-center leading-tight font-poppins'>
-          The Everwood <span className='font-bold'>{displayText}{currentIndex < wordToType.length && <span className="animate-pulse">|</span>}</span> <br />
-          <div className='h-[2px] w-4/5 lg:w-2/3 mx-auto bg-black lg:my-4 my-2'>
-          </div>
-          <div className='text-[20px] lg:text-[40px]'>
-            Bespoke Furniture
-          </div>
+    // <div data-aos="fade-up" data-aos-duration="2000">
+    <div className="-mt-15 lg:mt-[-50px]">
+      <div className="title text-center leading-tight font-poppins">
+        <div className="reveal-text">
+          <span className="font-bold">The Everwood Collection</span>
+        </div>
+
+        <div className="h-[2px] w-4/5 lg:w-2/3 mx-auto bg-black  my-1"></div>
+        <div className="reveal-text-down text-[20px] lg:text-[40px]">
+          <span>Bespoke Furniture</span>
         </div>
       </div>
     </div>
-  )
+    //   </div>
+  );
 }
 
-export default Section01
+export default Section01;
