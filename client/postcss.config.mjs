84ccf949fa0babfe59d8d628961b382416bd9852
sajitha00 <sajithageevinda.sd@gmail.com>
2026-01-06 10:31:12 +0530

    version update

--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,7 +1,13 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
 
-export default config;
+export default config;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
